e05565cde551169fa3c40bfd712ea8265997606e
hpz937 <hpz937@gmail.com>
2021-06-20 10:28:20 -0500

    Change probe offset for blokhead

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1150,7 +1150,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -5, -0.57 }
+#define NOZZLE_TO_PROBE_OFFSET { 48, -2, -0.57 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
